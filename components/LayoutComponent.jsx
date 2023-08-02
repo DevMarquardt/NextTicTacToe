91dfc889c2a000d7d9df0311b19e735ddbf725e0
GABRIEL FELLIPE MARQUARDT <gabriel_marquardt@estudante.sesisenai.org.br>
2023-08-01 21:07:47 -0300

    a

--- components/LayoutComponent.jsx
+++ components/LayoutComponent.jsx
@@ -1,23 +1,28 @@
 export default function Layout() {
+
+    function clica() {
+        console.log('clicou')
+    }
+
     return (
         <>
-
-            <div classname="flex space-x-1 flex flex-col flex items-center">
-                <div className="flex flex-col items-center">
+        
+            <div>
+                <div className="flex flex-nowrap justify-center">
                     <div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
                     </div>
                     <div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
                     </div>
                     <div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
-                        <div className="w-24 h-24 border-2 border-slate-950"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
+                        <div  onClick={() => clica()} className="w-24 h-24 border-2 border-slate-950 px-10"></div>
                     </div>
                 </div>
             </div>
